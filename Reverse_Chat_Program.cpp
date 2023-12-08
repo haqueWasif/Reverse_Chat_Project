a9da040e0ea1b0fc15e4e1fa9510be9e38d6e543
haqueWasif <123668473+haqueWasif@users.noreply.github.com>
2023-12-08 16:02:02 +0600

    Add files via upload

---
+++ Reverse_Chat_Program.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios_base :: sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    string s;
+    getline(cin, s);
+
+    stringstream ss(s);
+
+    vector<string> v;
+
+    string word;
+    while(ss >> word){
+        v.push_back(word);
+    }
+
+    for(auto &val : v){
+        reverse(val.begin(), val.end());
+    }
+
+    for(int i=v.size()-1; i>=0; i--){
+        cout << v[i] << " ";
+    }
+    cout << endl;
+
+
+    return 0;
+}
